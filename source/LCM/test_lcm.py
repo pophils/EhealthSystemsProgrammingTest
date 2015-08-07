2adc92e41e385f8f81493683825878150922e89b
pophils <kpophils@gmail.com>
2015-08-07 19:16:55 +0100

    Add LCM test

--- source/LCM/test_lcm.py
+++ source/LCM/test_lcm.py
@@ -1,1 +1,26 @@
-__author__ = 'pophils'
+
+
+import unittest
+from .lcm import LCM
+
+
+class LCMTestCase(unittest.TestCase):
+
+    def test_lcm_function_returns_an_empty_array_if_input_array_is_empty(self):
+        pass
+
+    def test_lcm_function_returns_first_integer_on_a_single_element_input_array(self):
+        pass
+
+    def test_lcm_returns_zero_if_all_the_input_array_elements_is_zero(self):
+        pass
+
+    def test_lcm_function_raises_type_error_if_input_array_contains_non_integer_element(self):
+        pass
+
+    def test_gcd_of_two_integers_returns_zero_if_one_of_the_integer_is_zero(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
