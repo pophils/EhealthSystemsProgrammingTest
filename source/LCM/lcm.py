60def6ce901c0c46233ff22f7e5ddb08d27d5bdb
pophils <kpophils@gmail.com>
2015-08-07 21:18:13 +0100

    Complete LCM and its test.

--- source/LCM/lcm.py
+++ source/LCM/lcm.py
@@ -1,1 +1,147 @@
-__author__ = 'pophils'
+
+
+class LCM(object):
+    """
+    Class provides implementation of the Least Common Multiple of one or more integers.
+    Initialize with a list of integers.
+
+    LCM(A,B) can be calculated efficiently by the division of (A * B) and GCD(A,B)
+    GCD (greatest common divisor) of two pairs of integers can be efficiently solved
+    by applying Euclidean Algorithm.
+
+    Call compute() to find the lcm of the initialized input integer list
+    """
+
+    def __init__(self, input_list):
+
+        if input_list is None:
+            raise ValueError('input list must not be none')
+
+        if not isinstance(input_list, list):
+            raise TypeError('input list must be a list type')
+
+        self.data = input_list
+
+    def gcd(self, a, b):
+        """
+        Function recursively finds the Greatest common divisor of two integers
+        using Euclidean algorithm
+        :param a: first integer
+        :param b: second integer
+        """
+        # base cases
+        if a == 0:
+            return b
+        if b == 0:
+            return a
+
+        return self.gcd(b, a % b)
+
+    def lcm(self, a, b):
+        """
+        Function finds the LCM of two integers
+        :param a: first integer
+        :param b: second integer
+        """
+        if a == 0:
+            return 0
+        if b == 0:
+            return 0
+
+        gcd = self.gcd(a, b)
+
+        return (a*b) // gcd
+
+    def compute(self):
+        """
+        Main class function that compute the LCM of an integer array.
+        """
+        # Edge cases
+        if len(self.data) == 0:
+            return []
+        if len(self.data) == 1:
+            return self.data[0]
+        if sum(self.data) == 0:
+            return 0
+
+        try:
+            current_lcm = self.lcm(self.data[0], self.data[1])
+            next_index = 2
+
+            while next_index < len(self.data) and current_lcm != 0:
+                current_lcm = self.lcm(current_lcm, self.data[next_index])
+                next_index += 1
+
+            return current_lcm
+        except TypeError:  # Edge case
+            raise TypeError('One of the list element is not an integer type.')
+
+
+def gcd(a, b):
+        """
+        Function recursively finds the Greatest common divisor of two integers
+        using Euclidean algorithm
+        :param a: first integer
+        :param b: second integer
+        """
+        # base cases
+        if a == 0:
+            return b
+        if b == 0:
+            return a
+
+        return gcd(b, a % b)
+
+
+def _lcm(a, b):
+        """
+        Function finds the LCM of two integers
+        :param a: first integer
+        :param b: second integer
+        """
+        if a == 0:
+            return 0
+        if b == 0:
+            return 0
+
+        _gcd = gcd(a, b)
+
+        return (a*b) // _gcd
+
+
+def lcm(input_array):
+        """
+        Function provides implementation of the Least Common Multiple of one or more integers.
+        LCM(A,B) can be calculated efficiently by the division of (A * B) and GCD(A,B)
+        GCD (greatest common divisor) of two pairs of integers can be solved
+        by applying Euclidean Algorithm.
+        """
+        # Edge cases
+        if input_array is None:
+            raise ValueError('input list must not be none')
+
+        if not isinstance(input_array, list):
+            raise TypeError('input list must be a list type')
+
+        if len(input_array) == 0:
+            return []
+        if len(input_array) == 1:
+            return input_array[0]
+        if sum(input_array) == 0:
+            return 0
+
+        try:
+            current_lcm = _lcm(input_array[0], input_array[1])
+            next_index = 2
+
+            while next_index < len(input_array) and current_lcm != 0:
+                current_lcm = _lcm(current_lcm, input_array[next_index])
+                next_index += 1
+
+            return current_lcm
+        except TypeError:  # Edge case
+            raise TypeError('One of the list element is not an integer type.')
+
+if __name__ == '__main__':
+    lcm = LCM([20, 120, 40])
+    print('LCM of %s is %d' % (lcm.data, lcm.compute()))
--- source/LCM/test_lcm.py
+++ source/LCM/test_lcm.py
@@ -1,25 +1,64 @@
 
 
 import unittest
-from .lcm import LCM
+from LCM.lcm import lcm, gcd, _lcm
 
 
 class LCMTestCase(unittest.TestCase):
 
-    def test_lcm_function_returns_an_empty_array_if_input_array_is_empty(self):
-        pass
+    def test_lcm_raises_value_error_on_undefined_input_array(self):
+        with self.assertRaises(ValueError):
+            lcm(None)
 
-    def test_lcm_function_returns_first_integer_on_a_single_element_input_array(self):
-        pass
+    def test_lcm_raises_type_error_on_a_non_list_type(self):
+        with self.assertRaises(TypeError):
+            lcm({})
+
+    def test_gcd_of_two_integers_returns_the_other_integer_if_one_of_the_integer_is_zero(self):
+        self.assertTrue(gcd(0, 1) == 1)
+        self.assertEqual(gcd(0, 0), 0)
+
+    def test_gcd_of_two_integers_returns_the_right_output(self):
+        self.assertTrue(gcd(10, 20) == 10)
+        self.assertTrue(gcd(12, 20) == 4)
+        self.assertTrue(gcd(5, 7) == 1)
+        self.assertTrue(gcd(36, 100) == 4)
+
+    def test_lcm_of_two_integers_returns_zero_if_at_least_one_integer_is_zero(self):
+        self.assertTrue(_lcm(0, 1) == 0)
+        self.assertEqual(_lcm(0, 0), 0)
+
+    def test_lcm_of_two_integers_returns_the_right_output(self):
+        self.assertTrue(_lcm(11, 4) == 44)
+        self.assertTrue(_lcm(4, 11) == 44)
+        self.assertTrue(_lcm(5, 2) == 10)
+        self.assertTrue(_lcm(2, 5) == 10)
+        self.assertTrue(_lcm(5, 202) == 1010)
+        self.assertTrue(_lcm(202, 5) == 1010)
+
+    def test_lcm_returns_an_empty_array_if_input_array_is_empty(self):
+        self.assertEqual(lcm([]), [])
+
+    def test_lcm_returns_first_integer_on_a_single_element_input_array(self):
+        self.assertEqual(lcm([100]), 100)
 
     def test_lcm_returns_zero_if_all_the_input_array_elements_is_zero(self):
-        pass
+        self.assertEqual(lcm([0, 0, 0, 0, 0, 0]), 0)
+
+    def test_lcm_compute_returns_correct_result_with_no_edge_case(self):
+        self.assertEqual(lcm([10, 5]), 10)
+
+        self.assertEqual(lcm([1, 0]), 0)
+
+        self.assertEqual(lcm([2, 4, 16]), 16)
+
+        self.assertEqual(lcm([2, 4, 16, 549]), 8784)
+
+        self.assertEqual(lcm([202, 0, 567]), 0)
 
     def test_lcm_function_raises_type_error_if_input_array_contains_non_integer_element(self):
-        pass
-
-    def test_gcd_of_two_integers_returns_zero_if_one_of_the_integer_is_zero(self):
-        pass
+        with self.assertRaises(TypeError):
+            self.assertEqual(lcm([2, 4, 16, 549, 'invalid input']), 8784)
 
 
 if __name__ == '__main__':
